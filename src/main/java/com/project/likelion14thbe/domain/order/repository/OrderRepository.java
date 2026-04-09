c8def9ff49d7f55e86c940857659e39be2bdaa45
sondg921 <sondg921@naver.com>
2026-04-09 20:10:51 +0900

    🔥️remove: 3주차 Jpa 의존성 삭제

--- src/main/java/com/project/likelion14thbe/domain/order/repository/OrderRepository.java
+++ src/main/java/com/project/likelion14thbe/domain/order/repository/OrderRepository.java
@@ -1,8 +1,7 @@
 package com.project.likelion14thbe.domain.order.repository;
 
-import com.project.likelion14thbe.domain.order.entity.Order;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository{
 }
--- src/main/java/com/project/likelion14thbe/domain/product/repository/ProductRepository.java
+++ src/main/java/com/project/likelion14thbe/domain/product/repository/ProductRepository.java
@@ -1,8 +1,7 @@
 package com.project.likelion14thbe.domain.product.repository;
 
-import com.project.likelion14thbe.domain.product.entity.Product;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository{
 }
