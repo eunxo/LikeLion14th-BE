72220a549c7651b3a635fadb979659553e7749f8
sondg921 <sondg921@naver.com>
2026-04-29 14:45:40 +0900

    ✨feat: 3주차 Product 관련 Controller, DTO 생성

--- src/main/java/com/project/likelion14thbe/domain/product/dto/request/ProductReqDTO.java
+++ src/main/java/com/project/likelion14thbe/domain/product/dto/request/ProductReqDTO.java
@@ -1,4 +1,12 @@
 package com.project.likelion14thbe.domain.product.dto.request;
 
 public class ProductReqDTO {
+    public record CreateProductReq(
+            String name,
+            Double price,
+            Integer quantity,
+            String description,
+            String productImage
+    ) {
+    }
 }
