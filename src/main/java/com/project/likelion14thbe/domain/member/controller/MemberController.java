da2b85a20e437cfcd2a50914fd9fa1eec4a24f0f
sondg921 <sondg921@naver.com>
2026-04-29 14:22:26 +0900

    ✨feat: 3주차 Member 관련 Controller, DTO 생성

--- src/main/java/com/project/likelion14thbe/domain/member/controller/MemberController.java
+++ src/main/java/com/project/likelion14thbe/domain/member/controller/MemberController.java
@@ -1,7 +1,22 @@
 package com.project.likelion14thbe.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.likelion14thbe.domain.member.dto.request.MemberReqDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "유저 API", description = "유저 관련 API")
+@RequestMapping("/api/v1")
 public class MemberController {
+
+    @PostMapping("/users/signup")
+    @Operation(summary = "회원 가입", description = "유저가 회원 가입을 합니다.")
+    public ResponseEntity<String> signup (
+            @RequestBody MemberReqDTO.UserSignupReq userSignupReq
+    ){
+        // 리뷰 생성 로직~~~
+        return ResponseEntity.ok("회원가입 성공");
+    }
 }
--- src/main/java/com/project/likelion14thbe/domain/member/dto/request/MemberReqDTO.java
+++ src/main/java/com/project/likelion14thbe/domain/member/dto/request/MemberReqDTO.java
@@ -1,4 +1,10 @@
 package com.project.likelion14thbe.domain.member.dto.request;
 
 public class MemberReqDTO {
+    public record UserSignupReq(
+            String name,
+            String email,
+            String password
+    ) {
+    }
 }
