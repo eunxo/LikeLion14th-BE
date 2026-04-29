b1101fb9efe5d3a9dd6535f59c8f2c500c7f89bf
kangho <kil0653@naver.com>
2026-04-30 03:18:57 +0900

    ✨ feat: Product 컨트롤러 및 dto 구현

--- src/main/java/com/project/likelion14thbe/domain/product/dto/request/ProductReqDTO.java
+++ src/main/java/com/project/likelion14thbe/domain/product/dto/request/ProductReqDTO.java
@@ -1,4 +1,22 @@
 package com.project.likelion14thbe.domain.product.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class ProductReqDTO {
+    public record ProductCreateReq(
+            Double price,
+            Long quantity,
+            String discription
+    ) {
+
+    }
+
+    public record ProductDeleteReq(
+            Double price,
+            Long quantity,
+            String discription
+    ) {
+
+    }
 }
--- src/main/java/com/project/likelion14thbe/domain/product/dto/response/ProductResDTO.java
+++ src/main/java/com/project/likelion14thbe/domain/product/dto/response/ProductResDTO.java
@@ -1,4 +1,37 @@
 package com.project.likelion14thbe.domain.product.dto.response;
 
+import com.project.likelion14thbe.domain.order.dto.response.OrderResDTO;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ProductResDTO {
+
+    @Builder
+    public record ProductGetRes(
+            List<ProductInfo> datalist
+    ) {
+
+        // 리스트 안에 들어갈 개별 주문 데이터
+        @Builder
+        public record ProductInfo(
+                Long productId,
+                String name,
+                Double price,
+                String photo
+        ) {
+        }
+    }
+
+    @Builder
+    public record ProductGetDeatilRes(
+            Long productId,
+            String name,
+            Double price,
+            float rating,
+            String description
+    ){
+
+    }
 }
