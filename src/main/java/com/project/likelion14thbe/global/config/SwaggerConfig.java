0ff573c327708ab1f8e7ce7a046fb174e5b01e26
gs07103 <gwonseok02@gmail.com>
2026-04-29 15:45:38 +0900

    ♻️refactor: SwaggerConfig 전역 인증 요구 제거 (인증 필요 API에만 선택 적용)

--- src/main/java/com/project/likelion14thbe/global/config/SwaggerConfig.java
+++ src/main/java/com/project/likelion14thbe/global/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.project.likelion14thbe.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    // http://localhost:8080/swagger-ui/index.html#/
+    // http://localhost:8081/swagger-ui/index.html#/
 
     @Bean
     public OpenAPI LikeLionAPI() {
@@ -21,10 +20,8 @@ public class SwaggerConfig {
                 .version("1.0.0"); //버전
 
         String jwtSchemeName = "JWT TOKEN";
-        // API 요청헤더에 인증정보 포함
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
-        // SecuritySchemes 등록
+        // JWT Bearer 인증 스키마 등록 (전역 요구는 걸지 않음 — 인증 필요 API에 @SecurityRequirement 명시)
         Components components = new Components()
                 .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
                         .name(jwtSchemeName)
@@ -35,7 +32,6 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
                 .info(info)
-                .addSecurityItem(securityRequirement)
                 .components(components);
     }
 }
