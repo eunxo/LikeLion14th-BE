cfd0d0774c36771ea97b9ba4c3d2e8afc15c52ae
sondg921 <sondg921@naver.com>
2026-04-30 20:50:12 +0900

    ✨feat: 4주차 Member 테이블 생성

--- src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
@@ -1,5 +1,34 @@
 package com.project.likelion14thbe.domain.member.entity;
 
-// @Entity
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "member")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "social_provider")
+    private String socialProvider;
+
+    @Column(name = "social_id")
+    private String socialId;
+
+    @Column(name = "profile_image")
+    private String profileImage;
 }
