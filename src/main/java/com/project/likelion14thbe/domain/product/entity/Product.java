77eaaaf5ed7f030239dfecddae6e0b81e94d8f9e
gs07103 <gwonseok02@gmail.com>
2026-05-01 13:34:07 +0900

    ✨feat: Product 엔티티 BaseEntity 상속 + ProductRepository JpaRepository
    - Product: @Entity/@Table(name="product"), id(IDENTITY) + name/price/category
      /description/thumbnailUrl, BaseEntity 상속으로 created_at/updated_at 자동
    - ProductRepository: JpaRepository<Product, Long> 상속,
      목록 조회용 findAllByOrderByCreatedAtDesc(Pageable) 추가

--- src/main/java/com/project/likelion14thbe/domain/product/entity/Product.java
+++ src/main/java/com/project/likelion14thbe/domain/product/entity/Product.java
@@ -1,5 +1,42 @@
 package com.project.likelion14thbe.domain.product.entity;
 
-//@Entity
-public class Product {
+import com.project.likelion14thbe.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "product")
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "category", nullable = false, length = 50)
+    private String category;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "thumbnail_url", length = 500)
+    private String thumbnailUrl;
 }
