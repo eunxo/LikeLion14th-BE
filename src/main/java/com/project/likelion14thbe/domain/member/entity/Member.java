50f785f0865745a0b9dd69619b7831ccb70f0a9f
gs07103 <gwonseok02@gmail.com>
2026-05-06 23:41:17 +0900

    ✨feat: Member 엔티티 BaseEntity 상속 + JPA 매핑
    - BaseEntity 상속, @Entity/@Table(name = "member")
    - id(IDENTITY) + name/email(unique)/password/profileImage 컬럼 정의
    - Builder/Getter/PROTECTED 기본 생성자

--- src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
@@ -1,5 +1,39 @@
 package com.project.likelion14thbe.domain.member.entity;
 
-//@Entity
-public class Member {
+import com.project.likelion14thbe.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "profile_image", length = 500)
+    private String profileImage;
 }
