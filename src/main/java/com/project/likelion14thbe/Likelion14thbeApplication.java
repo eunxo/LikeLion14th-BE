239a6c1cea175e5c069cffac6494b77d10171dbe
sondg921 <sondg921@naver.com>
2026-05-11 20:33:18 +0900

    ✨feat: 5주차 Application에 스케줄링 어노테이션 추가

--- src/main/java/com/project/likelion14thbe/Likelion14thbeApplication.java
+++ src/main/java/com/project/likelion14thbe/Likelion14thbeApplication.java
@@ -3,9 +3,11 @@ package com.project.likelion14thbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class Likelion14thbeApplication {
 
 	public static void main(String[] args) {
