fa5b1acdc04126b38ccd37ba424560f810d41f8b
julia <julia6317@naver.com>
2026-05-12 19:40:53 +0900

    ? feat: 5주차 실습 멤버필드

--- src/main/java/com/project/likelion14thbe/domain/member/service/command/MemberCommandServiceImpl.java
+++ src/main/java/com/project/likelion14thbe/domain/member/service/command/MemberCommandServiceImpl.java
@@ -4,6 +4,8 @@ import com.project.likelion14thbe.domain.member.converter.MemberConverter;
 import com.project.likelion14thbe.domain.member.dto.request.MemberReqDTO;
 import com.project.likelion14thbe.domain.member.dto.response.MemberResDTO;
 import com.project.likelion14thbe.domain.member.entity.Member;
+import com.project.likelion14thbe.domain.member.exception.MemberErrorCode;
+import com.project.likelion14thbe.domain.member.exception.MemberException;
 import com.project.likelion14thbe.domain.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +23,29 @@ public class MemberCommandServiceImpl implements MemberCommandService {
         memberRepository.save(member);
 
         return MemberConverter.toMemberResDTO(member);
+    }
+
+    @Override
+    public void updatePassword(Long memberId, MemberReqDTO.PasswordResetDTO dto) {
+        Member member = memberRepository.findByIdAndNotDeleted(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.updatePassword(dto.getPassword());
+    }
+
+    @Override
+    public void deleteMember(Long memberId) {
+        Member member = memberRepository.findByIdAndNotDeleted(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.delete();
+    }
+
+    @Override
+    public void updateMember(Long memberId, MemberReqDTO.UpdateReq updateReq) {
+        Member member = memberRepository.findByIdAndNotDeleted(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.updateMember(updateReq.getName(), updateReq.getProfileImage());
     }
 }
