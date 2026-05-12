d3036154a59da8db5918cbd61135fa93b1aa13e9
kangho <kil0653@naver.com>
2026-05-12 15:15:25 +0900

    ✨ feat: softDelete 되지 않은 상품 조회 메서드 구현

--- src/main/java/com/project/likelion14thbe/domain/product/repository/ProductRepository.java
+++ src/main/java/com/project/likelion14thbe/domain/product/repository/ProductRepository.java
@@ -2,8 +2,15 @@ package com.project.likelion14thbe.domain.product.repository;
 
 import com.project.likelion14thbe.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT m FROM Product m WHERE m.id = :id AND m.deletedAt IS NULL")
+    Optional<Product> findByAndNotDeleted(@Param("id") Long id);
 }
