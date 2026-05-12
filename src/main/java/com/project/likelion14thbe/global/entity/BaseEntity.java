a3951ecff9506402f694ec05e81ba893bac5ed7b
kangho <kil0653@naver.com>
2026-05-12 14:27:11 +0900

    ✨ feat: BaseEntity에 deletedAt 추가

--- src/main/java/com/project/likelion14thbe/global/entity/BaseEntity.java
+++ src/main/java/com/project/likelion14thbe/global/entity/BaseEntity.java
@@ -22,4 +22,11 @@ public class BaseEntity {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void delete(){
+        this.deletedAt = LocalDateTime.now();
+    }
 }
