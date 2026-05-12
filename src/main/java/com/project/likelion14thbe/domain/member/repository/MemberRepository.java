0bccd98bf916ec4f7cc203741df911808a5908ee
kangho <kil0653@naver.com>
2026-05-12 15:33:33 +0900

    ✨ feat: deletedAt이 30일 전보다 이전인 데이터 삭제 쿼리 구현

--- src/main/java/com/project/likelion14thbe/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/likelion14thbe/domain/member/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("SELECT m FROM Member m WHERE m.id = :id AND m.deletedAt IS NULL")
     Optional<Member> findByIdAndNotDeleted(@Param("id") Long id);
 
+    void deleteByDeletedAtBefore(LocalDateTime threshold);
 }
