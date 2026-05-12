686462fed5e14ead41cf84c2361eb46487cd753a
kangho <kil0653@naver.com>
2026-05-12 15:06:27 +0900

    ✨ feat: Product 수정 및 삭제 인터페이스 구현

--- src/main/java/com/project/likelion14thbe/domain/product/service/command/ProductCommandService.java
+++ src/main/java/com/project/likelion14thbe/domain/product/service/command/ProductCommandService.java
@@ -6,4 +6,8 @@ import com.project.likelion14thbe.domain.product.dto.response.ProductResDTO;
 public interface ProductCommandService {
 
     ProductResDTO.ProductCreateRes createProduct(ProductReqDTO.ProductCreateReq productCreateReq);
+
+    void updateProduct(Long ProductId, ProductReqDTO.ProductChangeDTO dto);
+
+    void deleteMember(Long productId);
 }
