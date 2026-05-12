7796e2785c243dc33cf9115d2c05fd52acbe3ef2
kangho <kil0653@naver.com>
2026-05-12 15:24:07 +0900

    ✨ feat: 멤버 비밀번호 변경 및 멤버삭제 컨트로러 구현

--- src/main/java/com/project/likelion14thbe/domain/member/service/command/MemberCommandServiceImpl.java
+++ src/main/java/com/project/likelion14thbe/domain/member/service/command/MemberCommandServiceImpl.java
@@ -4,6 +4,8 @@ import com.project.likelion14thbe.domain.member.converter.MemberConverter;
 import com.project.likelion14thbe.domain.member.dto.request.MemberReqDTO;
 import com.project.likelion14thbe.domain.member.dto.response.MemberResDTO;
 import com.project.likelion14thbe.domain.member.entity.Member;
+import com.project.likelion14thbe.domain.member.exception.MemberErrorCode;
+import com.project.likelion14thbe.domain.member.exception.MemberException;
 import com.project.likelion14thbe.domain.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +24,24 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         memberRepository.save(member);
 
         return MemberConverter.toMemberResponceDTO(member);
+    }
+
+    @Override
+    public void updatePassword(Long memberId, MemberReqDTO.PasswordResetDTO dto){
+        // 회원 정보 조회
+        Member member = memberRepository.findByIdAndNotDeleted(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        member.updatePassword(dto.password());
+    }
+
+    @Override
+    public void deleteMember(Long memberId){
+        //회원 정보 조회
+        Member member = memberRepository.findByIdAndNotDeleted(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        //soft delete 처리
+        member.delete();
     }
 }
