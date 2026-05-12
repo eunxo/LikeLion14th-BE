dd4d4d7a946a5d650c8ff6c973eb0963ca4705aa
kangho <kil0653@naver.com>
2026-05-12 14:58:00 +0900

    ✨ feat: softDelete 로 지워지지 않은 주문들 찾는 메서드 추가

--- src/main/java/com/project/likelion14thbe/domain/order/repository/OrderRepository.java
+++ src/main/java/com/project/likelion14thbe/domain/order/repository/OrderRepository.java
@@ -2,12 +2,18 @@ package com.project.likelion14thbe.domain.order.repository;
 
 import com.project.likelion14thbe.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByMemberId(Long memberId);
+
+    @Query("SELECT m FROM Order m WHERE m.id = :id AND m.deletedAt IS NULL")
+    Optional<Order> findByIdAndNotDeleted(@Param("id") Long id);
 }
