3b6896a0112fe10d13624cb2615ec20a2ab34d97
sondg921 <sondg921@naver.com>
2026-05-14 20:50:14 +0900

    ✨feat: 6주차 JwtUtil 생성

--- src/main/java/com/project/likelion14thbe/global/security/jwt/repository/TokenRepository.java
+++ src/main/java/com/project/likelion14thbe/global/security/jwt/repository/TokenRepository.java
@@ -1,4 +1,7 @@
 package com.project.likelion14thbe.global.security.jwt.repository;
 
-public interface TokenRepository {
+import com.project.likelion14thbe.global.security.jwt.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
 }
