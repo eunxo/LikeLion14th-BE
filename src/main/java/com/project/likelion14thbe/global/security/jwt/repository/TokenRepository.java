21ae05f9c2ae049a07d2946ded9ac832be0ce2ae
sondg921 <sondg921@naver.com>
2026-05-17 17:08:33 +0900

    ✨feat: 6주차

--- src/main/java/com/project/likelion14thbe/global/security/jwt/repository/TokenRepository.java
+++ src/main/java/com/project/likelion14thbe/global/security/jwt/repository/TokenRepository.java
@@ -3,5 +3,8 @@ package com.project.likelion14thbe.global.security.jwt.repository;
 import com.project.likelion14thbe.global.security.jwt.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByEmail(String email);
 }
