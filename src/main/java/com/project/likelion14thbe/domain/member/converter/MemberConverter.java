dc029f0658270e2e2c8ffeae92df6194ea197c20
kangho <kil0653@naver.com>
2026-05-17 22:41:46 +0900

    ✨feat: 일반 사용자 역할 추가

--- src/main/java/com/project/likelion14thbe/domain/member/converter/MemberConverter.java
+++ src/main/java/com/project/likelion14thbe/domain/member/converter/MemberConverter.java
@@ -3,16 +3,21 @@ package com.project.likelion14thbe.domain.member.converter;
 import com.project.likelion14thbe.domain.member.dto.request.MemberReqDTO;
 import com.project.likelion14thbe.domain.member.dto.response.MemberResDTO;
 import com.project.likelion14thbe.domain.member.entity.Member;
+import com.project.likelion14thbe.domain.member.enums.Role;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberConverter {
 
-    public static Member toMember(MemberReqDTO.MemberCreateReq memberCreateReq){
+
+    public static Member toMember(MemberReqDTO.MemberCreateReq memberCreateReq, BCryptPasswordEncoder passwordEncoder){
+        final String encodePassword = passwordEncoder.encode(memberCreateReq.password());
         return Member.builder()
                 .email(memberCreateReq.email())
-                .password(memberCreateReq.password())
+                .password(encodePassword)
+                .role(Role.ROLE_USER)
                 .name(memberCreateReq.name())
                 .build();
     }
