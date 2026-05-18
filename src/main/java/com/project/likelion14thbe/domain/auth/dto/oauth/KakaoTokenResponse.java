40211454f37dfc3edf73198174675f4a516d5c50
KWONSEOK02 <gwonseok02@gmail.com>
2026-05-18 21:29:04 +0900

    ✨feat: 소셜 신원 도메인과 카카오, 네이버 응답 DTO를 추가한다

---
+++ src/main/java/com/project/likelion14thbe/domain/auth/dto/oauth/KakaoTokenResponse.java
@@ -0,0 +1,14 @@
+package com.project.likelion14thbe.domain.auth.dto.oauth;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoTokenResponse(
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken,
+        @JsonProperty("expires_in") Integer expiresIn,
+        @JsonProperty("scope") String scope
+) {
+}
---
+++ src/main/java/com/project/likelion14thbe/domain/auth/enums/Provider.java
@@ -0,0 +1,6 @@
+package com.project.likelion14thbe.domain.auth.enums;
+
+public enum Provider {
+    KAKAO,
+    NAVER
+}
