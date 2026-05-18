7ca892cd4332ed0b0c10d628f76ce7f71c8584f1
eunxo <es2k1004@naver.com>
2026-05-18 14:18:03 +0900

    🐛fix : AuthException의 errorCode필드 제거

--- src/main/java/com/project/likelion14thbe/domain/auth/exception/AuthException.java
+++ src/main/java/com/project/likelion14thbe/domain/auth/exception/AuthException.java
@@ -1,20 +1,10 @@
 package com.project.likelion14thbe.domain.auth.exception;
 
 import com.project.likelion14thbe.global.apiPayload.exception.CustomException;
-import lombok.Getter;
 
-@Getter
 public class AuthException extends CustomException {
 
-    private final AuthErrorCode errorCode;
-
     public AuthException(AuthErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-
-    public AuthException(String message) {
-        super(message);
-        this.errorCode = null;
+        super(errorCode);
     }
 }
