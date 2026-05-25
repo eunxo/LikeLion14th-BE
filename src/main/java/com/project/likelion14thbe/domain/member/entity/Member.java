a3ffc7251950cb75d4f5f956fb162819da4c2de0
kangho <kil0653@naver.com>
2026-05-25 19:31:14 +0900

    🔥️ remove: password에 nullable 제거

--- src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
@@ -22,7 +22,7 @@ public class Member extends BaseEntity {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
     @Column(name = "photo")
