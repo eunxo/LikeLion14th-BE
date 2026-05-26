ddfdd367597cc5cb6cb383c07ca2f243a16ba8ad
julia <julia6317@naver.com>
2026-05-27 00:25:35 +0900

    ? feat: 7주차 : 과제 - naver 연동 로그인 구현

---
+++ src/main/java/com/project/likelion14thbe/domain/naver/controller/NaverLoginController.java
@@ -0,0 +1,43 @@
+package com.project.likelion14thbe.domain.naver.controller;
+
+import com.project.likelion14thbe.domain.naver.dto.response.NaverUserInfoResponseDTO;
+import com.project.likelion14thbe.domain.naver.service.NaverService;
+import com.project.likelion14thbe.global.apiPayload.CustomResponse;
+import com.project.likelion14thbe.global.security.jwt.dto.JwtDTO;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class NaverLoginController {
+
+    private final NaverService naverService;
+
+    @GetMapping("/auth/naver")
+    public CustomResponse<String> redirectToNaver(
+            final HttpServletResponse response
+    ) throws IOException {
+        naverService.redirectToNaver(response);
+        return CustomResponse.onSuccess(HttpStatus.FOUND, "naver redirect 완료");
+    }
+
+    @GetMapping("/naver/callback")
+    public CustomResponse<JwtDTO> callback(
+            @RequestParam("code") final String code,
+            @RequestParam("state") final String state
+    ) {
+        String accessToken = naverService.getAccessTokenFromNaver(code);
+        NaverUserInfoResponseDTO userInfo = naverService.getUserInfo(accessToken);
+
+        JwtDTO jwtDTO = naverService.processNaverLoginOrSignup(userInfo);
+
+        return CustomResponse.onSuccess(jwtDTO);
+    }
+}
---
+++ src/main/java/com/project/likelion14thbe/domain/naver/dto/response/NaverTokenResponseDTO.java
@@ -0,0 +1,15 @@
+package com.project.likelion14thbe.domain.naver.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record NaverTokenResponseDTO(
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires_in") String expiresIn,
+        @JsonProperty("error") String error,
+        @JsonProperty("error_description") String errorDescription
+) {
+}
