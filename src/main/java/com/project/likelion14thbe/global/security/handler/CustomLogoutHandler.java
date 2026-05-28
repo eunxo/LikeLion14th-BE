7be88e3f23213d9b92f705ea3015a1a015c74e1a
sondg921 <sondg921@naver.com>
2026-05-28 12:29:19 +0900

    ✨feat: 7주차 과제 트랜잭션 어노테이션 추가

--- src/main/java/com/project/likelion14thbe/global/security/handler/CustomLogoutHandler.java
+++ src/main/java/com/project/likelion14thbe/global/security/handler/CustomLogoutHandler.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,6 +24,7 @@ public class CustomLogoutHandler implements LogoutHandler {
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
+    @Transactional
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         log.info("[CustomLogoutHandler] 로그아웃 핸들러 동작 시작");
 
