ead201a7a6f1940c4dce02f4717c8b0a03c08e47
eunxo <es2k1004@naver.com>
2026-05-28 13:41:08 +0900

    fix : 네이버로그인 URL명세 통일

--- src/main/java/com/project/likelion14thbe/domain/naverlogin/controller/NaverLoginController.java
+++ src/main/java/com/project/likelion14thbe/domain/naverlogin/controller/NaverLoginController.java
@@ -7,14 +7,16 @@ import com.project.likelion14thbe.global.apiPayload.exception.CustomResponse;
 import com.project.likelion14thbe.global.security.jwt.dto.JwtDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.io.IOException;
 
 @Slf4j
 @Tag(name = "Naver Login", description = "네이버 소셜 로그인 API")
@@ -25,10 +27,13 @@ public class NaverLoginController {
 
     private final NaverService naverService;
 
-    @Operation(summary = "네이버 로그인 인증 URL 발급")
-    @GetMapping("/login-url")
-    public CustomResponse<Map<String, String>> getLoginUrl() {
-        return CustomResponse.onSuccess(Map.of("url", naverService.getAuthorizationUrl()));
+    @Operation(summary = "네이버 로그인 페이지로 redirect",
+            description = "호출 시 백엔드에서 네이버 로그인 인증 페이지로 302 redirect 처리합니다.")
+    @GetMapping
+    public void redirectToNaver(HttpServletResponse response) throws IOException {
+        String authorizationUrl = naverService.getAuthorizationUrl();
+        log.info("[NaverLoginController] 네이버 로그인 페이지로 redirect: {}", authorizationUrl);
+        response.sendRedirect(authorizationUrl);
     }
 
     @Operation(summary = "네이버 로그인 콜백 (회원가입/로그인 처리)")
